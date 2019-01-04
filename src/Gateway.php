2fb0af8b77009b5e701ae33cc1370476dc0cb385
Reüel van der Steege <reuel@pronamic.nl>
2019-01-04 16:33:12 +0100

    Happy 2019.

--- src/Gateway.php
+++ src/Gateway.php
@@ -12,7 +12,7 @@ use Pronamic\WordPress\Pay\Util as Pay_Util;
 /**
  * Title: WordPress pay WPMU DEV Membership gateway
  * Description:
- * Copyright: Copyright (c) 2005 - 2018
+ * Copyright: 2005-2019 Pronamic
  * Company: Pronamic
  *
  * @author  Remco Tolsma
--- src/ViewButton.php
+++ src/ViewButton.php
@@ -9,7 +9,7 @@ use Pronamic\WordPress\Pay\Util;
 /**
  * Title: WordPress pay WPMU DEV Membership view button
  * Description:
- * Copyright: Copyright (c) 2005 - 2018
+ * Copyright: 2005-2019 Pronamic
  * Company: Pronamic
  *
  * @author  Remco Tolsma
